5fe64d3bfde63a139e2ae3f86d98bdaba465e80b
aminoxlhd <aminoxlhd@gmail.com>
2024-08-09 22:01:52 +0100

    edit

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    if request.method == 'POST':
+        search_query = request.form.get('search', '')
+        min_price = request.form.get('min_price', None)
+        max_price = request.form.get('max_price', None)
+
+        query = Property.query
+        if search_query:
+            query = query.filter(Property.title.like(f'%{search_query}%'))
+        if min_price:
+            query = query.filter(Property.price >= min_price)
+        if max_price:
+            query = query.filter(Property.price <= max_price)
+
+        properties = query.all()
+    else:
+        properties = Property.query.all()
+    return render_template('index.html', properties=properties)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- views.py
+++ views.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from models import Property
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    properties = Property.query.all()
-    return render_template('index.html', properties=properties
+    if request.method == 'POST':
+        search_query = request.form['search']
+        properties = Property.query.filter(Property.title.like(f'%{search_query}%')).all()
+    else:
+        properties = Property.query.all()
+    return render_template('index.html', properties=properties)
